4c07a1504bd11e546a4d39c1f7a7170c71ee0ea8
Lucio Benfante <lucio@benfante.com>
2026-02-11 19:45:21 +0100

    Add GCD calculation example (functions and command line arguments)

---
+++ examples/programmin_rust_3rd/01_gcd/src/main.rs
@@ -0,0 +1,26 @@
+mod gcd;
+
+use gcd::gcd;
+use std::env;
+use std::process::exit;
+use std::str::FromStr;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {numbers:?} is {d}");
+}
